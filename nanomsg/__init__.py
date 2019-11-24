bc4cf8dead515ad1d992e1c18b96537dd74c4487
Stephen L Arnold <nerdboy@gentoo.org>
2019-11-23 22:54:10 -0800

    nanomsg/__init__.py: try a quick fix for set_int api change test failure
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- nanomsg/__init__.py
+++ nanomsg/__init__.py
@@ -370,10 +370,11 @@ class Socket(object):
                                value))
 
     def set_int_option(self, level, option, value):
+        vlen = 4
         buf = create_writable_buffer(Socket._INT_PACKER.size)
         Socket._INT_PACKER.pack_into(buf, 0, value)
         _nn_check_positive_rtn(wrapper.nn_setsockopt(self.fd, level, option,
-                                                     buf))
+                                                     buf, vlen))
 
     def get_int_option(self, level, option):
         size = Socket._INT_PACKER.size
